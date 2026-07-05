4503cba15526db4dab6191763b2cd5659ac62786
qingyun <108386582+536138283@users.noreply.github.com>
2026-07-06 00:44:23 +0800

    修复api检测问题
    修复api检测问题

--- uptime.config.ts
+++ uptime.config.ts
@@ -24,11 +24,11 @@ const workerConfig = {
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://status.furryfurry.cn/',
+      target: 'https://app.furryfurry.cn/healthz',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'furry百科服务健康检查',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://app.furryfurry.cn/healthz',
+      statusPageLink: 'https://furryfurry.cn,
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
       expectedCodes: [200],
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
