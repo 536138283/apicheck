3069459fad3db6cefaedf6aa96934b7d8a88b758
qingyun <108386582+536138283@users.noreply.github.com>
2026-07-06 14:21:41 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -46,7 +46,7 @@ const workerConfig = {
     },
     {
       // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'furry百科2',
+      id: '1',
       // `name` is used at status page and callback message
       name: 'furry百科2',
       // `method` should be a valid HTTP Method
